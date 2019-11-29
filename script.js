c521895263768b1dc5833c788b1b5616e1989671
Kuba Bergel <kubabergel@gmail.com>
2019-11-29 17:11:15 +0100

    hover

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
-    // scrollowanie na następny background-image
-    $('a[href^="#"]').on('click', function(event) {
-      const target = $($(this).attr('href'))
-  
-      if (target.length) {
-        event.preventDefault()
-        $('html, body').animate(
-          {
-            scrollTop: target.offset().top
-          },
-          1000
-        )
-      }
-    })
-  })
-  
-  
+  $('a[href^="#"]').on("click", function(event) {
+    const target = $($(this).attr("href"));
+
+    if (target.length) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+    }
+  });
+});
+
+// $(function() {
+//   $(window).on("scroll", function() {
+//       if($(window).scrollTop() > 100) {
+//           $(".menu").style('background-color', 'white');
+//       } else {
+//           $(".menu").style('background-color', 'red');
+//       }
+//   });
+// });
